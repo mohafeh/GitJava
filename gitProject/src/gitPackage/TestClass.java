f041414bd19707311e05b255b3d1d8eb8b9e3376
Fehmida Mohamedali <Fehmida.Mohamedali@uwl.ac.uk>
2014-08-06 08:14:57 +0100

    new version 4

--- gitProject/src/gitPackage/TestClass.java
+++ gitProject/src/gitPackage/TestClass.java
@@ -2,10 +2,10 @@ package gitPackage;
 
 public class TestClass
 {
-// new 3rd version
+// new 4th version
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World - Version new 3");
+		System.out.println("Hello World - Version new 4");
 	}
 
 }
